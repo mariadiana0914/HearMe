d7a86189ff6db1dc253141766e173b5aaf288e00
unknown <andrei.matei2010@gmail.com>
2020-05-04 18:48:23 +0300

    AccountDetailsSetup contracts

---
+++ app/src/main/java/com/girlesc/enguard/contracts/AccountDetailsSetupContract.java
@@ -0,0 +1,40 @@
+package com.girlesc.enguard.contracts;
+
+import com.girlesc.enguard.BasePresenter;
+import com.girlesc.enguard.BaseView;
+
+public interface AccountDetailsSetupContract {
+
+    interface View extends BaseView<Presenter> {
+        void setNextButtonEnabled(boolean enable);
+    }
+
+    interface AccountDetailsView extends BaseView<Presenter> {
+        String getPhoneNumber();
+
+        String getCountryCode();
+
+        String getFirstName();
+
+        String getLastName();
+
+        void showInvalidFirstName();
+
+        void showInvalidLastName();
+
+        void showInvalidPhoneNumber();
+    }
+
+    interface CodeVerificationView extends BaseView<Presenter> {
+        void showInvalidVerificationCode();
+    }
+
+    interface SecurityLevelView extends BaseView<Presenter> {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+
+}
