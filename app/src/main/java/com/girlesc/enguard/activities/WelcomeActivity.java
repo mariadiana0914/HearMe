b900eeef5b52cd69a7247d27088da4df401d5949
Nitescu Luminita <nitescu.luminita@gmail.com>
2020-05-04 16:37:11 +0300

    Facebook LogIn with Hashes bug

--- app/src/main/java/com/girlesc/enguard/activities/WelcomeActivity.java
+++ app/src/main/java/com/girlesc/enguard/activities/WelcomeActivity.java
@@ -8,7 +8,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.girlesc.enguard.R;
 import com.girlesc.enguard.utils.ToastUtils;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -21,9 +26,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
 
 public class WelcomeActivity extends AppCompatActivity {
     private Button logInBtn;
@@ -34,12 +42,43 @@ public class WelcomeActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 9001;
     private static final String TAG = "GoogleActivity";
     private FirebaseAuth mAuth;
-
+    private CallbackManager mCallbackManager;
+    private LoginButton mFacebookBtn;
+    private Button mFacebookIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        mCallbackManager = CallbackManager.Factory.create();
+        mFacebookBtn = findViewById(R.id.facebookLogInBtn);
+        mFacebookIcon = findViewById(R.id.facebookLogInIcon);
+        mFacebookBtn.setReadPermissions("email", "public_profile");
+        mFacebookBtn.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        mFacebookIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFacebookBtn.performClick();
+            }
+        });
 
         logInBtn = findViewById(R.id.logInBtn);
         logInBtn.setOnClickListener(new View.OnClickListener() {
@@ -79,6 +118,31 @@ public class WelcomeActivity extends AppCompatActivity {
 
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(WelcomeActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -91,6 +155,9 @@ public class WelcomeActivity extends AppCompatActivity {
             } catch (ApiException e) {
                 Log.w(TAG, getResources().getString(R.string.tv_google_sign_in_fail), e);
             }
+        }
+        else {
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
         }
     }
 
